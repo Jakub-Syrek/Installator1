24a610e3647db3f0fcdc2f3cb00824ccb89bf6f1
Jakub-Syrek <jakubvonsyrek@gmail.com>
2019-01-11 22:31:54 +0100

    2

---
+++ PS-GIT.ps1
@@ -0,0 +1,23 @@
+﻿# Install Chocolatey
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+# Install Chocolatey packages
+choco install git.install -y
+choco install conemu -y
+
+# Install PowerShell modules
+Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name 'posh-git'
+# Permanently add C:\Program Files\Git\usr\bin to machine Path variable
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\usr\bin", "Machine")
+
+# Creates profile if doesn't exist then edits it
+#if (!(Test-Path -Path $PROFILE)){ New-Item -Path $PROFILE -ItemType File } ; ise $PROFILE
+
+
+# Generate the key and put into the your user profile .ssh directory
+ssh-keygen -t rsa -b 4096 -C "jakubvonsyrek@gmail.com" -f $env:USERPROFILE\.ssh\id_rsa
+
+# Copy the public key. Be sure to copy the .pub for the public key
+Get-Content $env:USERPROFILE\.ssh\id_rsa.pub | clip
---
+++ SSH key.ps1
@@ -0,0 +1,20 @@
+﻿#Run in ConEmu !!!
+
+# Permanently add C:\Program Files\Git\usr\bin to machine Path variable
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Git\usr\bin", "Machine")
+
+# Generate the key and put into the your user profile .ssh directory
+#ssh-keygen -t rsa -b 4096 -C "jakubvonsyrek@gmail.com" -f $env:USERPROFILE\ssH\id_rsa.pub
+
+# Copy the public key. Be sure to copy the .pub for the public key
+#Get-Content $env:USERPROFILE\ssH\id_rsa.pub | clip
+Start-SshAgent
+Add-SshKey $env:USERPROFILE\ssh\id_rsa.pub
+
+git config --global user.email "jakubvonsyrek@gmail.com"
+git config --global user.name "Jakub-Syrek"
+git config --global push.default simple
+git config --global core.ignorecase false
+
+# Configure line endings for windows
+git config --global core.autocrlf true
